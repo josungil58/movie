ee85b8a79d0392fbef586d7641f89c149516a857
JO SUNG IL <josungil58@gmail.com>
2016-05-08 22:32:11 +0900

    1.005 on progress. not finished. modified according to a book named "android programming"

---
+++ app/src/main/java/com/example/android/movie/ImageAdapter.java
@@ -0,0 +1,56 @@
+package com.example.android.movie;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by josun on 2016-05-08.
+ */
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public ImageAdapter(MovieDownFragment movieDownFragment) {
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position){
+        return 0;
+    }
+    final String BASE_URL = "http://image.tmdb.org/t/p/";
+    final String SIZE = "w185/";
+    final String API_KEY ="?api_key=";
+    final String MY_API = "ff10e727a726c97c184d7ffd154b9cc0";
+    final String posterURL = BASE_URL + SIZE + API_KEY + MY_API;
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        if (convertView == null) {
+            imageView = (ImageView) convertView.findViewById(R.id.movie_poster_imageview);
+        }
+        else {
+            imageView = (ImageView) convertView;
+        }
+        Picasso.with(mContext).load(posterURL).into(imageView);
+        return imageView;
+    }
+}
--- app/src/main/java/com/example/android/movie/MovieDownFragment.java
+++ app/src/main/java/com/example/android/movie/MovieDownFragment.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 /**
  * Created by josun on 2016-05-05.
  */
 public class MovieDownFragment extends Fragment {
+
+    //private Context context;
+   // ArrayAdapter<Image> mMovieAdapter;
+
+    private ImageAdapter imageAdapter;
+    private String posterUrl;
 
     public MovieDownFragment() {
     }
@@ -23,9 +30,26 @@ public class MovieDownFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        ImageAdapter imageAdapter = new ImageAdapter(this);
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        //    GridView gridView = (GridView) rootView.findViewByID(R.id.gridview_movies);
+        GridView gridview = (GridView) rootView.findViewById(R.id.gridview_movies);
+        gridview.setAdapter(imageAdapter);
 
         return rootView;
     }
+
+/*    public class FetchMovieTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... params) {
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            ImageAdapter = new ImageAdapter(getActivity(), s);
+            ImageAdapter.notifyDataSetChanged();
+        }
+    }*/
+
 }
